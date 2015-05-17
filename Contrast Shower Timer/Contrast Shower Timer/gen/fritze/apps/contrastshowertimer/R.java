164ee8faaa26e9bbb0d189d569ccab9e8e80c86b
mfritze <mfritze@ualberta.ca>
2015-05-16 19:54:56 -0600

    Updated layout

--- Contrast Shower Timer/Contrast Shower Timer/gen/fritze/apps/contrastshowertimer/R.java
+++ Contrast Shower Timer/Contrast Shower Timer/gen/fritze/apps/contrastshowertimer/R.java
@@ -16,6 +16,11 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int button_green=0x7f050001;
+        public static final int grey=0x7f050000;
+        public static final int white=0x7f050002;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -24,31 +29,40 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int circle_button=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a000d;
-        public static final int buttonNewTimer=0x7f0a0000;
-        public static final int buttonRepeatTimer=0x7f0a0001;
-        public static final int buttonSetCycleDuration=0x7f0a0006;
-        public static final int buttonSetCyclesCount=0x7f0a0005;
-        public static final int buttonStartTimer=0x7f0a0002;
-        public static final int chronometerTimer=0x7f0a000b;
-        public static final int mainFragmentHolder=0x7f0a0009;
-        public static final int textViewCycleDuration=0x7f0a0004;
-        public static final int textViewCyclesCount=0x7f0a0003;
-        public static final int textViewTimerType=0x7f0a000c;
-        public static final int timer_layout=0x7f0a000a;
-        public static final int toggleButtonEndCycle=0x7f0a0008;
-        public static final int toggleButtonStartCycle=0x7f0a0007;
+        public static final int action_settings=0x7f0b0015;
+        public static final int buttonNewTimer=0x7f0b0000;
+        public static final int buttonRepeatTimer=0x7f0b0001;
+        public static final int buttonStartTimer=0x7f0b0002;
+        public static final int centerHorizontalLine=0x7f0b000f;
+        public static final int chronometerTimer=0x7f0b0013;
+        public static final int cyclesBar=0x7f0b0004;
+        public static final int durationBar=0x7f0b0007;
+        public static final int mainFragmentHolder=0x7f0b0011;
+        public static final int numberPickerCycles=0x7f0b0005;
+        public static final int numberPickerMinutes=0x7f0b0008;
+        public static final int numberPickerSeconds=0x7f0b000b;
+        public static final int numberPickerSecondsOrder=0x7f0b0009;
+        public static final int startBar=0x7f0b000d;
+        public static final int textViewCycleDuration=0x7f0b0006;
+        public static final int textViewCyclesCount=0x7f0b0003;
+        public static final int textViewMin=0x7f0b000a;
+        public static final int textViewSec=0x7f0b000c;
+        public static final int textViewTimerType=0x7f0b0014;
+        public static final int timer_layout=0x7f0b0012;
+        public static final int toggleButtonEndCycle=0x7f0b0010;
+        public static final int toggleButtonStartCycle=0x7f0b000e;
     }
     public static final class integer {
-        public static final int card_flip_time_full=0x7f060000;
-        public static final int card_flip_time_half=0x7f060001;
+        public static final int card_flip_time_full=0x7f070000;
+        public static final int card_flip_time_half=0x7f070001;
     }
     public static final class layout {
         public static final int finished_layout=0x7f030000;
@@ -57,24 +71,26 @@ public final class R {
         public static final int timer_layout=0x7f030003;
     }
     public static final class menu {
-        public static final int timer=0x7f090000;
+        public static final int timer=0x7f0a0000;
     }
     public static final class string {
-        public static final int action_settings=0x7f070002;
-        public static final int app_name=0x7f070000;
-        public static final int cold=0x7f070003;
-        public static final int end_cycle_cold=0x7f07000b;
-        public static final int end_cycle_hot=0x7f07000a;
-        public static final int get_ready=0x7f070005;
-        public static final int hello_world=0x7f070001;
-        public static final int hot=0x7f070004;
-        public static final int how_long=0x7f070007;
-        public static final int how_many_cycles=0x7f070006;
-        public static final int new_timer=0x7f07000d;
-        public static final int repeat=0x7f07000c;
-        public static final int start=0x7f070008;
-        public static final int start_cycle_cold=0x7f07000e;
-        public static final int start_cycle_hot=0x7f070009;
+        public static final int action_settings=0x7f080002;
+        public static final int app_name=0x7f080000;
+        public static final int cold=0x7f080003;
+        public static final int end_cycle_cold=0x7f08000b;
+        public static final int end_cycle_hot=0x7f08000a;
+        public static final int get_ready=0x7f080005;
+        public static final int hello_world=0x7f080001;
+        public static final int hot=0x7f080004;
+        public static final int how_long=0x7f080007;
+        public static final int how_many_cycles=0x7f080006;
+        public static final int min=0x7f08000f;
+        public static final int new_timer=0x7f08000d;
+        public static final int repeat=0x7f08000c;
+        public static final int sec=0x7f080010;
+        public static final int start=0x7f080008;
+        public static final int start_cycle_cold=0x7f08000e;
+        public static final int start_cycle_hot=0x7f080009;
     }
     public static final class style {
         /** 
@@ -98,10 +114,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
+        public static final int AppTheme=0x7f090001;
     }
 }
--- Contrast Shower Timer/Contrast Shower Timer/src/fritze/apps/contrastshowertimer/Fragments/NewTimerFragment.java
+++ Contrast Shower Timer/Contrast Shower Timer/src/fritze/apps/contrastshowertimer/Fragments/NewTimerFragment.java
@@ -1,7 +1,6 @@
 package fritze.apps.contrastshowertimer.Fragments;
 
 import android.app.Fragment;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,16 +8,21 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.NumberPicker.OnValueChangeListener;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 import fritze.apps.contrastshowertimer.R;
 import fritze.apps.contrastshowertimer.Timer;
-import fritze.apps.contrastshowertimer.TimerActivity;
 import fritze.apps.contrastshowertimer.Managers.GlobalFragmentManager;
 import fritze.apps.contrastshowertimer.Managers.TypefaceManager;
 
 public class NewTimerFragment extends Fragment{
 	private View view;
-	private Button startButton, cyclesButton, durationButton;
+	private Button startButton;
+	private NumberPicker cyclesPicker, minutesPicker, 
+			secondsPicker, secondsOrderPicker;
+	private TextView cyclesText, durationText;
 	private ToggleButton startToggle, endToggle;
 	private Timer timer;
 
@@ -34,23 +38,45 @@ public class NewTimerFragment extends Fragment{
 	@Override
 	public void onStart() {
 		super.onStart();
+		cyclesPicker = (NumberPicker) view.findViewById(R.id.numberPickerCycles);
+		cyclesPicker.setMinValue(0);
+		cyclesPicker.setMaxValue(20);
+		cyclesPicker.setWrapSelectorWheel(true);
+		cyclesPicker.clearFocus();
+		
+		
+		minutesPicker = (NumberPicker) view.findViewById(R.id.numberPickerMinutes);
+		minutesPicker.setMinValue(0);
+		minutesPicker.setMaxValue(59);
+		minutesPicker.setWrapSelectorWheel(true);
+		minutesPicker.clearFocus();
+		
+		secondsPicker = (NumberPicker) view.findViewById(R.id.numberPickerSeconds);
+		secondsPicker.setMinValue(0);
+		secondsPicker.setMaxValue(9);
+		secondsPicker.setWrapSelectorWheel(true);
+		secondsPicker.clearFocus();
+		
+		secondsOrderPicker = (NumberPicker) view.findViewById(R.id.numberPickerSecondsOrder);
+		secondsOrderPicker.setMinValue(0);
+		secondsOrderPicker.setMaxValue(5);
+		secondsOrderPicker.setWrapSelectorWheel(true);
+		secondsOrderPicker.clearFocus();
+		
+		
 		startButton = (Button) view.findViewById(R.id.buttonStartTimer);
 		startButton.setOnClickListener(startListener);
 		startButton.setTypeface(TypefaceManager.getTfLight(getActivity()));
 		
-		cyclesButton = (Button) view.findViewById(R.id.buttonSetCyclesCount);
-		cyclesButton.setOnClickListener(cyclesListener);
-		cyclesButton.setTypeface(TypefaceManager.getTfReg(getActivity()));
+		cyclesText = (TextView) view.findViewById(R.id.textViewCyclesCount);
+		cyclesText.setTypeface(TypefaceManager.getTfBold(getActivity()));
 		
-		durationButton = (Button) view.findViewById(R.id.buttonSetCycleDuration);
-		durationButton.setOnClickListener(durationListener);
-		durationButton.setTypeface(TypefaceManager.getTfReg(getActivity()));
-
+		durationText = (TextView) view.findViewById(R.id.textViewCycleDuration);
+		durationText.setTypeface(TypefaceManager.getTfBold(getActivity()));
 		
 		startToggle = (ToggleButton) view.findViewById(R.id.toggleButtonStartCycle);
 		startToggle.setTypeface(TypefaceManager.getTfReg(getActivity()));
 
-		
 		endToggle = (ToggleButton) view.findViewById(R.id.toggleButtonEndCycle);
 		endToggle.setTypeface(TypefaceManager.getTfReg(getActivity()));
 
@@ -74,9 +100,11 @@ public class NewTimerFragment extends Fragment{
 		}
 	};
 	
-	private OnClickListener cyclesListener;
+	private OnValueChangeListener cyclesListener;
 	
-	private OnClickListener durationListener;
+	private OnValueChangeListener minutesListener;
+	
+	private OnValueChangeListener secondsListener;
 
 
 
